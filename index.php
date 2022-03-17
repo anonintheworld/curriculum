83ae69e9706ba8d4295288f526f0f2a04496d537
anon32 <49528870+anonintheworld@users.noreply.github.com>
2022-03-17 12:19:16 -0600

    Create index.php
    La misma platilla html pero medificado de forma dinámica con PHP 8.0 donde mediante un arreglo tridimensional llamado $datos se pueden ir agregando y eliminado nuevos datos siguiendo la sintaxis del arreglo y estos se estarían renderizando de forma dinámica en el DOM al guardar y actualizar la pagina.

---
+++ index.php
@@ -0,0 +1,379 @@
+
+<?php
+    $datos=array(
+         "avatar" => "https://media-exp1.licdn.com/dms/image/C4E03AQGY_XNjUpYbHw/profile-displayphoto-shrink_800_800/0/1644938026316?e=1652918400&v=beta&t=HC7azkFhYK83N0wdA71ea_UtNlvqamB7M5izge9xDoo",
+         "title" => "Johan's CV",
+         "nombre" => "Ulises Reyes",
+         "perfil" => "Estudiante de ingeniería electrónica, desarrollador de android y web. Apasionada de la tecnología, con una gran capacidad de adaptación al cambio, habilidad y voluntad de aprender, responsable, creativo e innovador. Me encantan los retos porque con ellos siempre habrá una nueva meta a alcanzar.",
+         "oficio" => "Android Developer",
+         "contacto" => array(
+                      "numero" => array(
+                        "icon" => 'fa fa-phone',
+                        "text"=>'+52 712 201 22 94',
+                      ),
+                      "correo" => array(
+                        "icon" => 'fa fa-envelope-o',
+                        "text" => 'ulises.reyes404@gmail.com',
+                      ),
+                      "web" => array(
+                        "icon" => 'fa fa-globe',
+                        "text" => 'https://ulises-reyes.herokuapp.com',
+                      ),
+                      "linkedin" => array(
+                        "icon" => 'fa fa-linkedin-square',
+                        "text" => 'www.linkedin.com/in/ulises-espadin-reyes',
+                      ),
+                      "direccion" => array(
+                        "icon" => 'fa fa-map-marker',
+                        "text" => 'Atlacomulco, México',
+                      ),
+                    ),
+         "educaciones"    => array(
+                              "1" => array(
+                                "periodo" => "2018 - 2021",
+                                "formacion" => "Ingenieria en Sistemas Computacionales",
+                                "institucion" => "Universidad de America del Norte",
+                              ),
+                              "2" => array(
+                                "periodo" => "2014-2017",
+                                "formacion" => "Tecnico en Informatica",
+                                "institucion" => "Escuela Preparatoria Oficial No. 60",
+                              ),
+                      ),
+          "idiomas"    => array(
+                              "Español" => "98%",
+                              "Ingles" => "23%",
+                          ),
+          "habilidades_sociales"    => array(
+                                      "Trabajo en equipo" => "97%",
+                                      "Manejo de crisis" => "90%",
+                                      "Resolución de problemas" => "80%",
+                                      "Creatividad e innovación" => "90%",
+                                      "Análisis de información" => "85%",
+                                      "Aprendizaje continuo" => "90%",
+                                      "Capacidad de concentración" => "95%",
+                              ),
+         "experiencias"    => array(
+                              "1" => array(
+                                "periodo" => "2017 - Actualmente",
+                                "empresa" => "SmartGeeks",
+                                "puesto" => "Software Developer",
+                                "descripcion" => "Responsable de desarrollar aplicaciones web y móviles para android.",
+                              ),
+                              "2" => array(
+                                "periodo" => "05/2021 - 08/2021",
+                                "empresa" => "MERQUEO",
+                                "puesto" => "ANDROID DEVELOPER",
+                                "descripcion" => "Desarrollador de aplicaciones para android, responsable de agregar nuevas funcionalidades a la aplicación.",
+                              ),
+                              "3" => array(
+                                "periodo" => "10/2020 - 02/2021",
+                                "empresa" => "SCUMMERS",
+                                "puesto" => "ANDROID DEVELOPER",
+                                "descripcion" => "Desarrollador android a cargo de implementar las nuevas características de la aplicación VendoPorChat.",
+                              ),
+                              "4" => array(
+                                "periodo" => "06/2017 - 10/2017",
+                                "empresa" => "MIREGION",
+                                "puesto" => "MOBILE DEVELOPER",
+                                "descripcion" => "Responsable del desarrollo de aplicaciones móviles an android para el medio de noticias MiRegion.",
+                              ),
+          
+                            ),                                                         
+         "certificados" => array(
+                              "1" => array(
+                                "diploma" => "ENGLISH DOT WORKS LEVEL 1 - INGLES 1",
+                                "institucion" => "Servicio Nacional de Aprendizaje – SENA",
+                                "periodo" => "06/2015 – 07/2015",
+                              ),
+                              "2" => array(
+                                "diploma" => "ENGLISH DOT WORKS LEVEL 1 - INGLES 2",
+                                "institucion" => "Servicio Nacional de Aprendizaje – SENA 2",
+                                "periodo" => "06/2015 – 07/2015",
+                              ),
+          ),
+         "habilidades_profesionales" => array(
+                              "html" => "98%",
+                              "css" => "58%",
+                              "javascript" => "50%",
+                              "php" => "50%",
+                              "java" => "30%",
+                              "python" => "30%",
+                              "react" => "40%",
+                              "sql" => "60%",
+                              "basesdedatos" => "23%",
+                              "diseño" => "23%",
+                              "marketing" => "23%",
+                              "ciberseguridad" => "23%",
+                              "photoshop" => "23%",
+                              "python" => "23%",
+                            ),
+          "intereses" => array(
+                        "1" => array(
+                              "icon" => 'fa fa-headphones',
+                              "hobby"=>'Musica',
+                        ),
+                        "2" => array(
+                              "icon" => 'fa fa-laptop',
+                              "hobby"=>'Tecnologia',
+                        ),
+                        "3" => array(
+                              "icon" => 'fa fa-book',
+                              "hobby"=>'Leer',
+                        ),
+                        "4" => array(
+                              "icon" => 'fa fa-book',
+                              "hobby"=>'Leer',
+                        ),
+                        "5" => array(
+                              "icon" => 'fa fa-motorcycle',
+                              "hobby"=>'Viajar',
+                        ),
+                        "6" => array(
+                              "icon" => 'fa fa-bicycle',
+                              "hobby"=>'Ejercicio',
+                        ),
+          ),  
+    );
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css"
+      integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <title><?php echo $datos["title"];?></title>
+  </head>
+  <body>
+    <div class="container">
+      <!--Aside section-->
+      <aside class="left_side">
+        <div class="profile_text">
+          <div class="profile_img">
+            <img
+              src="<?php echo $datos["avatar"];?>"
+              alt="Ulises Reyes"
+            />
+          </div>
+          <h2><?php echo $datos["nombre"];?><br /><span><?php echo $datos["oficio"];?></span></h2>
+        </div>
+
+        <div class="contact_info">
+          <h3 class="title">Información de Contacto</h3>
+          <ul>
+            <?php
+            foreach($datos["contacto"] as $contacto => $dato){
+             echo '
+              <li>
+                <span class="icon"><i class="'.$dato["icon"].'" aria-hidden="true"></i></span>
+                <span class="text">'.$dato["text"].'</span>
+              </li>
+            ';
+            }
+           ?>
+           <!--  <li>
+              <span class="icon"
+                ><i class="fa fa-phone" aria-hidden="true"></i>
+              </span>
+              <span class="text">+57 3242 235 623</span>
+            </li> -->
+          </ul>
+        </div>
+
+        <div class="contact_info education">
+          <h3 class="title">Educación</h3>
+          <ul>
+            <?php
+                  foreach($datos["educaciones"] as $educacion){
+                      echo '
+                      <li>
+                        <h5>'.$educacion["periodo"].'</h5>
+                        <h4>
+                        '.$educacion["formacion"].'
+                        </h4>
+                        <h4>'.$educacion["institucion"].'</h4>
+                      </li>
+                      ';
+                  }
+            ?>
+            <!-- <li>
+              <h5>2018 - 2021</h5>
+              <h4>
+                  Ingenieria en Sistemas Computacionales
+              </h4>
+              <h4>Universidad de America del Norte</h4>
+            </li> -->
+          </ul>
+        </div>
+
+        <div class="contact_info language">
+          <h3 class="title">Habilidades sociales</h3>
+          <ul>
+            <?php
+                  foreach($datos["habilidades_sociales"] as $habilidad => $porsentaje){
+                        echo '
+                          <li>
+                            <span class="text">'.$habilidad.'</span>
+                            <span class="percent">
+                              <div style="width:'.$porsentaje.'"></div>
+                            </span>
+                          </li>
+                        ';
+                  }
+            ?>
+           <!--  <li>
+              <span class="text">Capacidad de concentración </span>
+              <span class="percent">
+                <div style="width: 80%"></div>
+              </span>
+            </li> -->
+          </ul>
+        </div>
+
+        <div class="contact_info language">
+          <h3 class="title">Idiomas</h3>
+          <ul>
+          <?php
+                  foreach($datos["idiomas"] as $idioma => $porsentaje){
+                        echo '
+                          <li>
+                            <span class="text">'.$idioma.'</span>
+                            <span class="percent">
+                              <div style="width:'.$porsentaje.'"></div>
+                            </span>
+                          </li>
+                        ';
+                  }
+            ?>
+            <!-- <li>
+              <span class="text">Español</span>
+              <span class="percent">
+                <div style="width:70%"></div>
+              </span>
+            </li> -->
+          </ul>
+        </div>
+      </aside>
+
+      <!-- Right section -->
+      <main class="right_side">
+        <section class="about">
+          <h2 class="title">Perfil</h2>
+          <p>
+            <?php echo $datos["perfil"];?>
+          </p>
+        </section>
+        <section class="about">
+          <h2 class="title">Experiencia</h2>
+
+          <?php
+                foreach( $datos["experiencias"] as $experiencia ){
+                    echo '
+                      <div class="box">
+                          <div class="year_company">
+                            <h5>'.$experiencia["periodo"].'</h5>
+                            <h5>'.$experiencia["empresa"].'</h5>
+                          </div>
+                          <div class="description">
+                            <h4>'.$experiencia["puesto"].'</h4>
+                            <p>
+                            '.$experiencia["descripcion"].'
+                            </p>
+                          </div>
+                      </div>
+                    ';
+                }
+          ?>
+          <!-- <div class="box">
+            <div class="year_company">
+              <h5>06/2017 - 10/2017</h5>
+              <h5>MiRegion</h5>
+            </div>
+            <div class="description">
+              <h4>Mobile Developer</h4>
+              <p>
+                Responsable del desarrollo de aplicaciones móviles an android
+                para el medio de noticias MiRegion.
+              </p>
+            </div>
+          </div> -->
+        </section>
+
+        <div class="about">
+          <h2 class="title">Certificados</h2>
+
+          <?php
+                 foreach($datos["certificados"] as $certificado){
+                      echo '
+                          <div class="description certificates">
+                            <h4>'.$certificado["diploma"].'</h4>
+                            <p>'.$certificado["institucion"].'</p>
+                            <span>'.$certificado["periodo"].'</span>
+                          </div>
+                      ';
+                 }
+          ?>
+         <!--
+          <div class="description certificates">
+            <h4>Introducción a la Visión Artificial con Python</h4>
+            <p>Universidad Colombiana</p>
+            <span>Marzo 2019</span>
+          </div> -->
+        </div>
+
+       
+
+    
+
+        <div class="about">
+          <h2 class="title">Habilidades Profesionales</h2>
+          <?php
+                 foreach($datos["habilidades_profesionales"] as $habilidad => $porsentaje){
+                      echo '
+                        <div class="box">
+                          <h4>'.$habilidad.'</h4>
+                          <div class="percent">
+                            <div style="width:'.$porsentaje.'"></div>
+                          </div>
+                        </div>
+                      ';
+                 }
+          ?>
+         <!--  <div class="box">
+            <h4>HTML</h4>
+            <div class="percent">
+              <div style="width: <?php echo $datos["habilidades_profesionales"]["html"];?>"></div>
+            </div>
+          </div> -->
+          
+        </div>
+
+        <div class="about interest">
+          <h2 class="title">Intereses</h2>
+          <ul>
+          <?php
+               
+                  foreach($datos["intereses"] as $interes => $dato){
+                    echo '
+                        <li><i class="'.$dato["icon"].'" aria-hidden="true"></i>'.$dato["hobby"].'</li>
+                      ';
+                  }
+           ?>
+            <!-- <li><i class="fa fa-motorcycle" aria-hidden="true"></i> Viajar</li> -->
+          </ul>
+        </div>
+      </main>
+    </div>
+  </body>
+</html>
